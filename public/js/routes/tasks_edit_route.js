ad19e4288e9548e78a02efe31219034f6294d8ff
Marcell Monteiro Cruz <0000marcell@gmail.com>
2015-11-06 21:25:10 -0200

    monir changes

--- public/js/routes/tasks_edit_route.js
+++ public/js/routes/tasks_edit_route.js
@@ -2,7 +2,7 @@ App.TasksEditRoute = Ember.Route.extend({
   model: function(params) {
     return this.store.find('task', params.task_id);
   },
-  // Rooll back if the user transitions away by clicking a link, clicking the
+  // Roolll back if the user transitions away by clicking a link, clicking the
   // browser's back button, or otherwise.
   deactivate: function() {
     var model = this.modelFor('tasks.edit');
