8173ae9649b6cd62c1244e380eee73703ef65cab
Marcell Monteiro Cruz <0000marcell@gmail.com>
2015-11-07 13:56:27 -0200

    minor changes

--- public/js/views/tasks_statistics_view.js
+++ public/js/views/tasks_statistics_view.js
@@ -1,6 +1,7 @@
 App.TasksStatisticsView= Ember.View.extend({
   didInsertElement: function(){
     appWindow.resize(900, 640);
+    console.log("test");
     App.Task.store.find('task').then(function(tasks){
       statistics.getStatistics(tasks, 7); 
       statistics.loadD3Calendar(tasks);
