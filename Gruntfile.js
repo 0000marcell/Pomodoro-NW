93a484313e09e71da178aa439146a4733e3f1de8
Marcell Monteiro Cruz <0000marcell@gmail.com>
2015-11-08 17:59:20 -0200

    making the no-aws version the default version

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,5 +23,5 @@ module.exports = function(grunt) {
   });
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-nw-builder');
-  grunt.registerTask('default', ['jade']);
+  grunt.registerTask('default', ['jade', 'nwjs']);
 };
--- public/js/helpers/aws_helper.js
+++ public/js/helpers/aws_helper.js
@@ -3,11 +3,11 @@ function read_aws_info(){
   try{
     content = fs.readFileSync("aws_config.json");
     if (content == 'undefined'){
-      alert(" aws config file was empty, plz enter your aws settings");   
+      // alert(" aws config file was empty, plz enter your aws settings");   
     }
     var obj = JSON.parse(content);
     return obj;
   }catch(err){
-    alert('An Error occured when trying to read the file '+this.file+" error : "+err);
+    // alert('An Error occured when trying to read the file '+this.file+" error : "+err);
   }  
 }
