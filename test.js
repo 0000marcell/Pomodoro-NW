fece52e7e61d09571fcc4034dde71ea6aebb4947
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-01-21 20:10:05 -0200

    rewriting statistics functions with chain methods call

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-let date = new Date('11/27/2016');
-console.log('date: ', date.toString());
+let obj = { name: null, sirname: null};
+
+let funcs = {
+  addName(name){
+    this.name = name;
+    return this;
+  },
+  addSirName(sirname){
+    this.sirname = sirname;
+    return this;
+  },
+  print(){
+    console.log(`${this.name} ${this.sirname}`);
+    return this;
+  }
+}
+
+let objFunc = Object.assign(obj, funcs);
+
+objFunc.addName('marcell').addSirName('cruz').print();
