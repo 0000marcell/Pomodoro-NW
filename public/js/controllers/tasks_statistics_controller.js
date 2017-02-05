0b1257ad25fd7c276b789d032ac95745b6ebe455
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-02-05 10:45:18 -0200

    error when loading statistics #2

--- public/js/controllers/tasks_statistics_controller.js
+++ public/js/controllers/tasks_statistics_controller.js
@@ -30,6 +30,51 @@ App.TasksStatisticsController = Ember.ObjectController.extend({
   mpMonth2016: null,
   mpMonth2017: null,
   mpDay2015: null,
+  init(){
+    let arr = [],
+        allTask = {id: 'all', name: 'all'};
+    this.store.findAll('task').then((tasks) => {
+      statistics.init(tasks)
+        .filterPomodoros();
+      this.set('tasks', tasks);
+      arr = tasks.slice(0);
+      this.set('tasksList', arr);
+      this.get('tasksList')
+        .unshiftObject(allTask);
+      this.set('selectedTask', allTask);
+      let currentDate = new Date().getFullYear(),
+        first = statistics.firstPomodoro().getFullYear(),
+        diff = currentDate - first + 1,
+        years = Array.from(new Array(diff), (x,i) => i + first);
+      this.set('years', years);
+      this.set('yearStart', years[0]);
+      this.set('monthStart', this.get('months').objectAt(0));
+      this.set('yearEnd', years[years.length - 1]);
+      this.set('monthEnd', this.get('months').objectAt(this.get('months.length') - 1));
+      statistics.loadBarChart()
+                .loadD3Calendar();
+      this.set('mpMonth2015', 
+        statistics.mostProductiveMonth(2015));
+      this.set('mpMonth2016', 
+        statistics.mostProductiveMonth(2016));
+      this.set('mpMonth2017', 
+        statistics.mostProductiveMonth(2017));
+      this.set('mpDay2015', 
+        statistics.mostProductiveDay(2015));
+      this.set('mpDay2016', 
+        statistics.mostProductiveDay(2016));
+      this.set('mpDay2017', 
+        statistics.mostProductiveDay(2017));
+      let todayPomodoros = statistics.resetFilter().todayPomodoros();
+      this.set('todayPomodoros', todayPomodoros);
+      this.set('todayTotal', 
+          `${this.get('todayPomodoros').filterBy('name', 'total')[0].time}h`);
+      let weekPomodoros = statistics.resetFilter().weekPomodoroH();
+      this.set('weekPomodoros', weekPomodoros);
+      this.set('weekTotal', 
+          `${weekPomodoros.filterBy('name', 'total')[0].time}h`);
+    });
+  },
   actions: { 
     calculateStatistics(){
       let selectedIds = (this.get('selectedTask.id') !== 'all') ? [this.get('selectedTask.id')] :
--- test.js
+++ test.js
@@ -1,34 +1,39 @@
-controller.get('tasksList')
+statistics.init(tasks)
+              .filterPomodoros();
+    this.set('tasks', tasks);
+    arr = tasks.slice(0);
+    this.set('tasksList', arr);
+    this.get('tasksList')
       .unshiftObject(allTask);
-controller.set('selectedTask', allTask);
-let currentDate = new Date().getFullYear(),
-  first = statistics.firstPomodoro().getFullYear(),
-  diff = currentDate - first + 1,
-  years = Array.from(new Array(diff), (x,i) => i + first);
-controller.set('years', years);
-controller.set('yearStart', years[0]);
-controller.set('monthStart', controller.get('months').objectAt(0));
-controller.set('yearEnd', years[years.length - 1]);
-controller.set('monthEnd', controller.get('months').objectAt(controller.get('months.length') - 1));
-statistics.loadBarChart()
-          .loadD3Calendar();
-controller.set('mpMonth2015', 
-  statistics.mostProductiveMonth(2015));
-controller.set('mpMonth2016', 
-  statistics.mostProductiveMonth(2016));
-controller.set('mpMonth2017', 
-  statistics.mostProductiveMonth(2017));
-controller.set('mpDay2015', 
-  statistics.mostProductiveDay(2015));
-controller.set('mpDay2016', 
-  statistics.mostProductiveDay(2016));
-controller.set('mpDay2017', 
-  statistics.mostProductiveDay(2017));
-let todayPomodoros = statistics.resetFilter().todayPomodoros();
-controller.set('todayPomodoros', todayPomodoros);
-controller.set('todayTotal', 
-    `${controller.get('todayPomodoros').filterBy('name', 'total')[0].time}h`);
-let weekPomodoros = statistics.resetFilter().weekPomodoroH();
-controller.set('weekPomodoros', weekPomodoros);
-controller.set('weekTotal', 
-    `${weekPomodoros.filterBy('name', 'total')[0].time}h`);
+    this.set('selectedTask', allTask);
+    let currentDate = new Date().getFullYear(),
+      first = statistics.firstPomodoro().getFullYear(),
+      diff = currentDate - first + 1,
+      years = Array.from(new Array(diff), (x,i) => i + first);
+    this.set('years', years);
+    this.set('yearStart', years[0]);
+    this.set('monthStart', this.get('months').objectAt(0));
+    this.set('yearEnd', years[years.length - 1]);
+    this.set('monthEnd', this.get('months').objectAt(this.get('months.length') - 1));
+    statistics.loadBarChart()
+              .loadD3Calendar();
+    this.set('mpMonth2015', 
+      statistics.mostProductiveMonth(2015));
+    this.set('mpMonth2016', 
+      statistics.mostProductiveMonth(2016));
+    this.set('mpMonth2017', 
+      statistics.mostProductiveMonth(2017));
+    this.set('mpDay2015', 
+      statistics.mostProductiveDay(2015));
+    this.set('mpDay2016', 
+      statistics.mostProductiveDay(2016));
+    this.set('mpDay2017', 
+      statistics.mostProductiveDay(2017));
+    let todayPomodoros = statistics.resetFilter().todayPomodoros();
+    this.set('todayPomodoros', todayPomodoros);
+    this.set('todayTotal', 
+        `${this.get('todayPomodoros').filterBy('name', 'total')[0].time}h`);
+    let weekPomodoros = statistics.resetFilter().weekPomodoroH();
+    this.set('weekPomodoros', weekPomodoros);
+    this.set('weekTotal', 
+        `${weekPomodoros.filterBy('name', 'total')[0].time}h`);
