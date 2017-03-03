6f60b349eefa9158a6b3c129500fb7e262603257
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-03-03 17:14:57 -0300

    creating compile methods for tests and the normal app

--- public/compile.js
+++ public/compile.js
@@ -1,14 +1,24 @@
 let fs = require('fs'),
-    jsdom = require('jsdom'),
-    htmlSource = fs.readFileSync("index.html", "utf8");
+    jsdom = require('jsdom');
+
+param = process.argv[2];
+
+if(param =='test'){
+  baseFile = './tests.html'
+}else{
+  baseFile = './app.html'
+}
+
+let htmlSource = fs.readFileSync(baseFile, "utf8");
 
 jsdom.env(htmlSource, (err, window) => {
   let app = window.document.getElementById('ember-app');
+  app.innerHTML = "";
   fs.readdir('./templates', (err, filenames) => {
     let fileContent;
     filenames.forEach((fileName) => {
       app.innerHTML += fs.readFileSync(`./templates/${fileName}`); 
     });
-    fs.writeFileSync('./compiled.html', window.document.documentElement.outerHTML);
+    fs.writeFileSync(baseFile, window.document.documentElement.outerHTML);
   });
 });
