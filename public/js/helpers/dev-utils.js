c938ee89c1a9bab4195970c19b137743322c5692
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-03-24 16:58:28 -0300

    generating tasks for development mode

---
+++ public/js/helpers/dev-utils.js
@@ -0,0 +1,12 @@
+function generateRandomTasks(){
+  let json = {
+    id: null,
+    name: null,
+    creation_date: null, 
+    last_active: null,
+    duration: null,
+    pomodoros: []
+  }
+  for (var i = 0; i < 10; i++) {
+  }
+}
--- public/js/models/task.js
+++ public/js/models/task.js
@@ -74,23 +74,3 @@ App.Task = DS.Model.extend({
     return arr[0]+' '+arr[1]+'h'+arr[2]+'m';
   }
 });
-
-App.resetFixtures = function() {
-  fileIO.set('file', mainDataPath);
-  App.Task.FIXTURES = $.map(fileIO.read(), 
-                    function(el) { return el; }); 
-  var params = {Key: 'data.json'};
-  if(awsUseStorage){
-    bucket.getObject(params, function(error, data) {
-      if (error) {
-        alert("File sync failed : "+error);
-      } else {
-        var attachment = data.Body.toString();
-        App.Task.FIXTURES = $.map(JSON.parse(attachment), 
-                      function(el) { return el; }); 
-      }
-    });
-  }
-};
-
-App.resetFixtures();
