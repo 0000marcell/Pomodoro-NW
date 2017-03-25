e1555fca4c8382b08541e17a473768dca4abc1f8
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-03-25 20:26:35 -0300

    pushing record directly to the store

--- public/js/routes/application.js
+++ public/js/routes/application.js
@@ -1,5 +1,12 @@
 App.ApplicationRoute = Ember.Route.extend({
   model: function() {
+    this.store.push('tasks', {
+        id: 1,
+        title: "Fewer Moving Parts",
+        artist: "David Bazan",
+        songCount: 10
+        
+    });
     return this.store.findAll('task');
   },
   afterModel(){
