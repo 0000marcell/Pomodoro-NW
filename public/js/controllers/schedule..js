7585abfe8ceb885af4eaba3250181d9fd7a0ae4b
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-04-19 16:50:07 -0300

    [WIP] creating schedule funtionality, creating page

--- public/js/controllers/schedule..js
+++ public/js/controllers/schedule..js
@@ -1,4 +1,8 @@
 App.ScheduleController = Ember.ObjectController.extend({
+  schedule: [{day: 'monday', tasks: [{id: 1, name: 'task1', pomodoros: 3}, 
+             {id: 2, name: 'task2', pomodoros: 4}]}, 
+             {day: 'tuesday', tasks: [{id: 3, name: 'task3', pomodoors: 5},
+             {id: 4, name: 'task4', pomodoros: 5}]}],
   totalTime: 0,
   tasks: [],
   daysOfTheWeek: [],
