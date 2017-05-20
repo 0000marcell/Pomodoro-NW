65591d8c8595dbe78510e7b6ace62ac85d285fde
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-05-20 16:33:48 -0300

    testing written for sidenav-panel

--- pomodoro-electron/tests/integration/components/sidenav-panel-test.js
+++ pomodoro-electron/tests/integration/components/sidenav-panel-test.js
@@ -5,21 +5,32 @@ moduleForComponent('sidenav-panel', 'Integration | Component | sidenav panel', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('#sidenav-panel-01 it opens the panel', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('openSidenav', true);
+  this.render(hbs`{{sidenav-panel 
+    openSidenav=openSidenav}}`);
+  assert.equal(this.$('.open-sidenav').length , 1);
+});
 
-  this.render(hbs`{{sidenav-panel}}`);
+test('#sidenav-panel-02 opens left panel', function(assert){
+  this.set('leftPanel', true);
+  this.render(hbs`{{sidenav-panel 
+    leftPanel=leftPanel}}`);
+  assert.equal(this.$('.left-panel').length , 1);
+});
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#sidenav-panel}}
-      template block text
-    {{/sidenav-panel}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('#sidenav-panel-03 closes the panel on overlay click', 
+  function(assert){
+  this.set('leftPanel', true); 
+  this.set('openSidenav', true);
+  this.render(hbs`{{sidenav-panel 
+    leftPanel=leftPanel
+    openSidenav=openSidenav
+    }}`);
+  this.$('#sp-test-overlaybtn').click();
+  assert.equal(this.$('.open-sidenav').length , 0);
+  assert.equal(this.$('.left-panel').length , 0);
 });
