0ff387af04a399d8690c0e2a645d8b84c512e584
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-06-05 18:52:09 -0300

    new clock style changed

--- pomodoro-electron/app/components/clock-comp.js
+++ pomodoro-electron/app/components/clock-comp.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['clock-comp'],
+  classNames: ['clock-comp', 'column'],
   min: '00',
   sec: '00',
   didReceiveAttrs(){
